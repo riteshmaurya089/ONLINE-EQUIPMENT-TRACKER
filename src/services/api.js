76f4a6c172e7bfa07eef0644f3289fb992cbb73a
Ritesh Maurya <ayanmaurya89@gmail.com>
2025-12-25 02:06:17 +0530

    done

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,12 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL ,
+  baseURL: "https://equipment-tracker-backen.onrender.com/api",
 });
 
 export const fetchEquipment = () => API.get("/equipment");
 export const addEquipment = (data) => API.post("/equipment", data);
-export const updateEquipment = (id, data) => API.put(`/equipment/${id}`, data);
-export const deleteEquipment = (id) => API.delete(`/equipment/${id}`);
+export const updateEquipment = (id, data) =>
+  API.put(`/equipment/${id}`, data);
+export const deleteEquipment = (id) =>
+  API.delete(`/equipment/${id}`);
